2b4f1f263c635f151187415844f709e3a2de6ac6
liuxinyuk <31117897+liuxinyuk@users.noreply.github.com>
2018-01-25 11:30:39 +0800

    Update ROI.py

--- ROI.py
+++ ROI.py
@@ -9,3 +9,9 @@ import time
 ROI = np.zeros(frame.shape, np.uint8)      #感兴趣区域ROI
 cv2.circle(ROI, (cx,cy), d, (255, 255, 255), -1)   #画白圆,圆心(cx,cy),直径，-1是填充
 imgroi= ROI & frame            #图像位与
+
+
+#也不同三个通道一起做，用opencv的函数
+ROI = np.zeros(frame.shape[:2], dtype = "uint8")   #单通道灰度图
+cv2.circle(ROI, (cx,cy), d, 255, -1)    #画白园
+imgroi = cv2.bitwise_and(frame, frame, mask=ROI)
